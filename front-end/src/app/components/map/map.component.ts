a00e402e89c53f51adc63a264ac25f59d8fcec1d
orentin <colapala@gmail.com>
2020-04-17 18:08:57 +0200

    j'en ai marre d'etre un fdp putin

--- front-end/src/app/components/map/map.component.ts
+++ front-end/src/app/components/map/map.component.ts
@@ -80,208 +80,6 @@ export class MapComponent implements OnInit {
   'Deux-Sèvres' ,
   'Vienne' ,
   'Haute-Vienne' ,
-  'Ariège' ,
-  'Aude' ,
-  'Aveyron' ,
-  'Gard' ,
-  'Haute-Garonne' ,
-  'Gers' ,
-  'Hérault' ,
-  'Lot' ,
-  'Lozère' ,
-  'Hautes-Pyrénées' ,
-  'Pyrénées-Orientales' ,
-  'Tarn' ,
-  'Tarn-et-Garonne' ,
-  'Ain' ,
-  'Allier' ,
-  'Ardèche' ,
-  'Cantal' ,
-  'Drôme' ,
-  'Isère' ,
-  'Loire' ,
-  'Haute-Loire' ,
-  'Puy-de-Dôme' ,
-  'Rhône' ,
-  'Savoie' ,
-  'Haute-Savoie' ,
-  'Alpes-de-Haute-Provence' ,
-  'Hautes-Alpes' ,
-  'Alpes-Maritimes' ,
-  'Bouches-du-Rhône' ,
-  'Var' ,
-  'Vaucluse' ,
-  'Corse-du-Sud' ,
-  'Haute-Corse' ,
-  'Guadeloupe' ,
-  'Martinique' ,
-  'Guyane' ,
-  'La Réunion' ,
-  'Mayotte' ,
-  'Paris' ,
-  'Seine-et-Marne' ,
-  'Yvelines' ,
-  'Essonne' ,
-  'Hauts-de-Seine' ,
-  'Seine-Saint-Denis' ,
-  'Val-de-Marne' ,
-  'Val-d’Oise' ,
-  'Cher' ,
-  'Eure-et-Loir' ,
-  'Indre' ,
-  'Indre-et-Loire' ,
-  'Loir-et-Cher' ,
-  'Loiret' ,
-  'Côte-d’Or' ,
-  'Doubs' ,
-  'Jura' ,
-  'Nièvre' ,
-  'Haute-Saône' ,
-  'Saône-et-Loire' ,
-  'Yonne' ,
-  'Territoire de Belfort' ,
-  'Calvados' ,
-  'Eure' ,
-  'Manche' ,
-  'Orne' ,
-  'Seine-Maritime' ,
-  'Aisne' ,
-  'Nord' ,
-  'Oise' ,
-  'Pas-de-Calais' ,
-  'Somme' ,
-  'Ardennes' ,
-  'Aube' ,
-  'Marne' ,
-  'Haute-Marne' ,
-  'Meurthe-et-Moselle' ,
-  'Meuse' ,
-  'Moselle' ,
-  'Bas-Rhin' ,
-  'Haut-Rhin' ,
-  'Vosges' ,
-  'Loire-Atlantique' ,
-  'Maine-et-Loire' ,
-  'Mayenne' ,
-  'Sarthe' ,
-  'Vendée' ,
-  'Côtes-d’Armor' ,
-  'Finistère' ,
-  'Ille-et-Vilaine' ,
-  'Morbihan' ,
-  'Charente' ,
-  'Charente-Maritime' ,
-  'Corrèze' ,
-  'Creuse' ,
-  'Dordogne' ,
-  'Gironde' ,
-  'Landes' ,
-  'Lot-et-Garonne' ,
-  'Pyrénées-Atlantiques' ,
-  'Deux-Sèvres' ,
-  'Vienne' ,
-  'Haute-Vienne' ,
-  'Ariège' ,
-  'Aude' ,
-  'Aveyron' ,
-  'Gard' ,
-  'Haute-Garonne' ,
-  'Gers' ,
-  'Hérault' ,
-  'Lot' ,
-  'Lozère' ,
-  'Hautes-Pyrénées' ,
-  'Pyrénées-Orientales' ,
-  'Tarn' ,
-  'Tarn-et-Garonne' ,
-  'Ain' ,
-  'Allier' ,
-  'Ardèche' ,
-  'Cantal' ,
-  'Drôme' ,
-  'Isère' ,
-  'Loire' ,
-  'Haute-Loire' ,
-  'Puy-de-Dôme' ,
-  'Rhône' ,
-  'Savoie' ,
-  'Haute-Savoie' ,
-  'Alpes-de-Haute-Provence' ,
-  'Hautes-Alpes' ,
-  'Alpes-Maritimes' ,
-  'Bouches-du-Rhône' ,
-  'Var' ,
-  'Vaucluse' ,
-  'Corse-du-Sud' ,
-  'Haute-Corse' ,
-  'Guadeloupe' ,
-  'Martinique' ,
-  'Guyane' ,
-  'La Réunion' ,
-  'Mayotte' ,
-  'Paris' ,
-  'Seine-et-Marne' ,
-  'Yvelines' ,
-  'Essonne' ,
-  'Hauts-de-Seine' ,
-  'Seine-Saint-Denis' ,
-  'Val-de-Marne' ,
-  'Val-d’Oise' ,
-  'Cher' ,
-  'Eure-et-Loir' ,
-  'Indre' ,
-  'Indre-et-Loire' ,
-  'Loir-et-Cher' ,
-  'Loiret' ,
-  'Côte-d’Or' ,
-  'Doubs' ,
-  'Jura' ,
-  'Nièvre' ,
-  'Haute-Saône' ,
-  'Saône-et-Loire' ,
-  'Yonne' ,
-  'Territoire de Belfort' ,
-  'Calvados' ,
-  'Eure' ,
-  'Manche' ,
-  'Orne' ,
-  'Seine-Maritime' ,
-  'Aisne' ,
-  'Nord' ,
-  'Oise' ,
-  'Pas-de-Calais' ,
-  'Somme' ,
-  'Ardennes' ,
-  'Aube' ,
-  'Marne' ,
-  'Haute-Marne' ,
-  'Meurthe-et-Moselle' ,
-  'Meuse' ,
-  'Moselle' ,
-  'Bas-Rhin' ,
-  'Haut-Rhin' ,
-  'Vosges' ,
-  'Loire-Atlantique' ,
-  'Maine-et-Loire' ,
-  'Mayenne' ,
-  'Sarthe' ,
-  'Vendée' ,
-  'Côtes-d’Armor' ,
-  'Finistère',
-  'Ille-et-Vilaine' ,
-  'Morbihan' ,
-  'Charente' ,
-  'Charente-Maritime' ,
-  'Corrèze' ,
-  'Creuse' ,
-  'Dordogne' ,
-  'Gironde' ,
-  'Landes' ,
-  'Lot-et-Garonne' ,
-  'Pyrénées-Atlantiques' ,
-  'Deux-Sèvres' ,
-  'Vienne' ,
-  'Haute-Vienne' ,
   'Ariège',
   'Aude' ,
   'Aveyron' ,
@@ -339,6 +137,9 @@ export class MapComponent implements OnInit {
   mousOverReg = new Object();
   mousLeaveReg = new Object();
 
+  mousOverDept = new Object();
+  mousLeaveDept = new Object();
+
   constructor(private mapService: MapService) {}
 
   ngOnInit(): void {
@@ -350,6 +151,7 @@ export class MapComponent implements OnInit {
     this.isRegion = !this.isRegion;
     console.log(this.isRegion);
     if (this.isRegion){
+      this.removeDeptListener();
       this.initializeMapReg();
     } else{
        this.removeRegListener();
@@ -383,18 +185,24 @@ export class MapComponent implements OnInit {
         if (elementAttributes) {
           const dataNom = elementAttributes.getNamedItem('data-nom');
           if ( dataNom && dataNom.value === depName) {
-            depElement.addEventListener('mouseover',
-              function(){
-                        depElement.style.fillOpacity = '0.7';
-                        depElement.style.stroke = 'white';
-                        depElement.style.strokeOpacity = '0.8';
-                      } );
-            depElement.addEventListener('mouseleave',
-              function(){
-                        depElement.style.fillOpacity = '1';
-                        depElement.style.stroke = color;
-                      } );
+            let mousOver = function() {
+              depElement.style.fillOpacity = '0.7';
+              depElement.style.stroke = 'white';
+              depElement.style.strokeOpacity = '0.8';
+            };
+            depElement.addEventListener('mouseover', mousOver );
+            this.mousOverDept[dataNom.value] = mousOver;
+
+            let mousLeave = function(){
+              depElement.style.fillOpacity = '1';
+              depElement.style.stroke = color;
+            };
+            depElement.addEventListener('mouseleave', mousLeave );
+            this.mousLeaveDept[dataNom.value] = mousLeave;
+
             depElement.style.fill = color;
+            depElement.style.fillOpacity = '1';
+            depElement.style.stroke = color;
           }
         }
       }
@@ -469,6 +277,39 @@ export class MapComponent implements OnInit {
     }
     this.mousOverReg = new Object();
     this.mousLeaveReg = new Object();
+  }
+
+  removeDeptListener(): void {
+    const pathElements = document.getElementsByTagName('path');
+    for (const element in pathElements) {
+      const depElement = pathElements[element];
+      const elementAttributes = depElement.attributes;
+
+      if (elementAttributes) {
+        const dataNom = elementAttributes.getNamedItem('data-nom');
+        if (dataNom ) {
+          depElement.removeEventListener('mouseover', this.mousOverDept[dataNom.value]);
+          depElement.removeEventListener('mouseleave', this.mousLeaveDept[dataNom.value]);
+        }
+      }
+    }
+    /*
+    const pathElements = document.getElementsByTagName('path');
+    let i=0;
+    for (const element in pathElements) {
+      const depElement = pathElements[element];
+      const elementAttributes = depElement.attributes;
+
+      if (elementAttributes) {
+        const dataNom = elementAttributes.getNamedItem('data-nom');
+        if (dataNom) {
+          depElement.removeEventListener('mouseover', this.mousOverDept[dataNom.value]);
+          depElement.removeEventListener('mouseleave', this.mousLeaveDept[dataNom.value]);
+        }
+      }
+    }*/
+    this.mousOverDept = new Object();
+    this.mousLeaveDept = new Object();
   }
 
   assignColor(nb){
