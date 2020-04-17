56afaccb6a50de3f12eb5b6ee0af0916659d1543
Grazia <grazia.ribbeni@gmail.com>
2020-04-17 12:03:22 +0200

    graphique fake pour la présentation

--- front-end/src/app/components/right-bar/right-bar.component.ts
+++ front-end/src/app/components/right-bar/right-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as Highcharts from 'highcharts';
 
 @Component({
   selector: 'app-right-bar',
@@ -7,9 +8,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RightBarComponent implements OnInit {
 
+  // @ts-ignore
+  // @ts-ignore
+  public options: any = {
+    Chart: {
+      type: 'area',
+      height: 100,
+      width: 100
+    },
+    title: {
+      text: 'Evolution'
+    },
+    credits: {
+      enabled: false
+    },
+    xAxis: {
+      categories: ['1750', '1800', '1850', '1900', '1950', '1999', '2050'],
+      tickmarkPlacement: 'on',
+      title: {
+        enabled: false
+      }
+    },
+    series: [{
+      name: 'Cas confirmés',
+      data: [502, 635, 809, 947, 1402, 3634, 5268]
+    }, {
+      name: 'Guéris',
+      data: [163, 203, 276, 408, 547, 729, 628]
+    }, {
+      name: 'Hospitalisés',
+      data: [10, 31, 50, 156, 339, 300, 280]
+    }, {
+      name: 'Décès',
+      data: [18, 31, 54, 156, 339, 818, 1201]
+    }
+    ]
+  };
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    Highcharts.chart('click', this.options);
   }
 
 }
