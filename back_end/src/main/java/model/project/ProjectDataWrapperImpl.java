0e425216b00a74e05c0f47f4f3b650ea585c1d6e
cmc373 <milinaire.c@gmail.com>
2020-04-29 12:56:42 +0200

    start simulation in wrapper transform data to sir parameters

--- back_end/src/main/java/model/project/ProjectDataWrapperImpl.java
+++ back_end/src/main/java/model/project/ProjectDataWrapperImpl.java
@@ -1,14 +1,15 @@
 package model.project;
 
+import com.google.common.collect.Iterables;
 import lombok.Getter;
 import model.data.DayData;
 import model.io.DataScrapperImpl;
+import model.simulator.SIRSimulator;
+import model.simulator.Simulator;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.time.LocalDate;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -28,9 +29,24 @@ public class ProjectDataWrapperImpl implements ProjectDataWrapper {
   private ProjectData projectData = new ProjectDataImpl();
 
   /**
+   * The {@link model.simulator.Simulator} to simulate the propagation of the
+   * COVID-19.
+   */
+  private Simulator simulator = new SIRSimulator();
+  /**
    * Id for france to access data.
    */
   private static final String FRA = "FRA";
+
+  /**
+   * Population of France.
+   */
+  private static final double POPULATION_FRA = 67000000.0;
+
+  /**
+   * Comparator used to compare dates as string.
+   */
+  private final DateComparator dateComparator = new DateComparator();
 
   @Override
   public void getCurrentAllDataFrance() throws IOException {
@@ -85,11 +101,11 @@ public class ProjectDataWrapperImpl implements ProjectDataWrapper {
   @Override
   public List<DayData> infosDept(final String date) {
     final Map<String, Map<String, DayData>> locations =
-            projectData.getLocations().entrySet().stream().filter(
-                    map -> map.getKey()
-                    .contains("DEP")).
-                    collect(Collectors.toMap(Map.Entry::getKey,
-                            Map.Entry::getValue));
+      projectData.getLocations().entrySet().stream().filter(
+        map -> map.getKey()
+          .contains("DEP")).
+        collect(Collectors.toMap(Map.Entry::getKey,
+          Map.Entry::getValue));
     final List<DayData> res = new ArrayList<>(locations.size());
     for (Map<String, DayData> map : locations.values()) {
       res.add(map.get(date));
@@ -103,6 +119,72 @@ public class ProjectDataWrapperImpl implements ProjectDataWrapper {
       projectData.getLocations();
     localisations.computeIfAbsent(key, k -> new HashMap<>());
   }
+
+  @Override
+  public DayData simulateFrance(final String date) {
+    final double dead = getDead(FRA);
+    final double recovered = getRecovered(FRA);
+    final double susceptible = getSusceptible(FRA);
+    final double infectious = 1 - susceptible;
+    SIRSimulator simulator = new SIRSimulator(susceptible, infectious,
+      recovered,
+      dead);
+    simulator.step();
+    final double deadNew = Iterables.getLast(simulator.getDead());
+    final double recoveredNew = Iterables.getLast(simulator.getRecovered());
+    final double susceptibleNew = Iterables.getLast(simulator.getSusceptible());
+    final double infectiousNew = Iterables.getLast(simulator.getInfectious());
+    final DayData dayData = new DayData();
+    dayData.setTotalDeaths((int) (deadNew * POPULATION_FRA));
+    dayData.setRecoveredCases((int) (recovered * POPULATION_FRA));
+    dayData.setTotalCases((int) (POPULATION_FRA - (susceptibleNew * POPULATION_FRA)));
+    return dayData;
+  }
+
+  private DayData getLatestData(final String location) {
+    final Map<String, Map<String, DayData>> localisations =
+      projectData.getLocations();
+    final Map<String, DayData> franceMap = localisations.get(FRA);
+    final Optional<String> latestDate =
+      franceMap.keySet().stream().max(dateComparator);
+    if (latestDate.isEmpty()) {
+      throw new IllegalStateException("No max date in map");
+    }
+    return franceMap.get(latestDate.get());
+  }
+
+  private double getSusceptible(final String location) {
+    final DayData latestData = getLatestData(location);
+    final int infectedPersons = latestData.getTotalCases()
+      - latestData.getRecoveredCases() - latestData.getTotalDeaths();
+    return infectedPersons / POPULATION_FRA;
+  }
+
+  private double getRecovered(final String location) {
+    final DayData latestData = getLatestData(location);
+    final double latestDataRecoveredCases = latestData.getRecoveredCases();
+    final double latestDataTotalCases = latestData.getTotalCases();
+    return latestDataRecoveredCases / latestDataTotalCases;
+  }
+
+  private double getDead(final String location) {
+    final DayData latestData = getLatestData(location);
+    final int latestDead = latestData.getTotalDeaths();
+    return latestDead / POPULATION_FRA;
+  }
+
+  /**
+   * Simple class to compare to string dates.
+   */
+  private static class DateComparator implements Comparator<String> {
+    @Override
+    public int compare(final String s, final String t1) {
+      LocalDate date1 = LocalDate.parse(s);
+      LocalDate date2 = LocalDate.parse(t1);
+      return date1.compareTo(date2);
+    }
+  }
+
 }
 
 
